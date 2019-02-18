fe99b3d6d958effc088db7f36630a4b76c3c49a4
James Stephenson <c4p7.fl1n7@gmail.com>
2019-02-18 17:15:48 -0500

    added argument parsing to cycletest.py

--- cycletest.py
+++ cycletest.py
@@ -1,21 +1,42 @@
+#!/usr/bin/env python3
+
 from pontifex import Deck
-import random
+import random, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--false-positive', action='store_true')
+decksource = parser.add_mutually_exclusive_group()
+decksource.add_argument('-k', '--key')
+decksource.add_argument('-d', '--deck')
+parser.add_argument('-l', '--length', type=int, default=1000000)
+args = parser.parse_args()
+
+print('Checking for cycles of length less than or equal to', args.length)
 
 decks = []
 
 deck = Deck()
 
-while len(decks) < 2000000:
+if args.deck:
+    f = open(args.deck, 'r')
+    deck = Deck(f.read())
+    f.close()
+
+if type(args.key) is str:
+    deck.key(args.key)
+
+while len(decks) < args.length:
     decks.append(deck.cards)
     deck.nextNumber()
 
-def test():
+def false_positive():
     
     random.seed()
     decks.append(deck.cards)
     random.shuffle(decks)
 
-#test()
+if args.false_positive:
+    false_positive()
 
 if deck.cards in decks:
     print('Cycle detected')
